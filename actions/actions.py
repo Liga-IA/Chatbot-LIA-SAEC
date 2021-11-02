0d0661f0a105a18ccd6d6952a8c1fb8918f7a62f
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-11-02 19:40:28 -0300

    Adicionei as alterações do Alisson

--- actions/actions.py
+++ actions/actions.py
@@ -9,15 +9,15 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from pprint import pprint
 
-class ActionHelloWorld(Action):
+class registraong(Action):
 
     def name(self) -> Text:
         return "action_register_ong"
 
     def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        pprint(tracker)
-        dispatcher.utter_message(text="Registrando...")
-
-        return []
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        nome = tracker.get_slot('ong_name')
+        contatos = ' Whatsapp: ' + str(tracker.get_slot('ong_whatsapp')) + ' Email: ' + str(tracker.get_slot('ong_email'))
+        local = 'cidade de ' + str(tracker.get_slot('ong_city')) + ', ' + str(tracker.get_slot('ong_uf'))
+        dispatcher.utter_message(text ='registrando a ONG ' + str(nome) + '\n da ' + local + "\n contatos: " + contatos)
