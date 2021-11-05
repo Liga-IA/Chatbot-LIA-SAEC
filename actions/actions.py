8962a48f81fe50b8a01a0b3fc9c1c1f07fc97c61
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2021-11-05 14:43:52 +0000

    adicionei os forms e deletei o caso

--- actions/actions.py
+++ actions/actions.py
@@ -2,7 +2,7 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-API_PATH = "https://3333-aquamarine-warbler-xbftz3tt.ws-us17.gitpod.io"
+API_PATH = "https://3333-aquamarine-warbler-xbftz3tt.ws-us18.gitpod.io"
 
 from typing import Any, Text, Dict, List
 
@@ -45,11 +45,12 @@ class ActionRegistraOng(Action):
         r = requests.post(API_PATH+ '/ongs', data = json.dumps(pload),headers=headers)  
         
 
-
+        print(r.text)
         if(int(r.status_code) != 200):
             dispatcher.utter_message(text="Erro ao criar a ong.. tente novamente mais tarde")
         else:
             dispatcher.utter_message(text ='registrando a ONG ' + str(nome) + '\n da ' + local + "\n contatos: " + contatos +"\n\n" + "O código da sua ong é: " + r.json()['id'])
+        return []
 
 
 class ActionExclusionSelect(Action):
@@ -80,8 +81,42 @@ class ActionExclusionSelect(Action):
 
             btns = []
             for i in range(len(cases)):
-                btns.append({"payload":"/delete_specific_case","title":cases[i]["title"]})
+                btns.append({"payload":'/delete_specific_case{"case_id":"'+str(cases[i]["id"])+'"}',"title":cases[i]["title"]})
             btns.append({"payload":"/cancel_operation","title":"Nenhum"})
             dispatcher.utter_message(text="Escolha qual caso quer excluir", buttons=btns)
         else:
             dispatcher.utter_message(text ="Id não informado, ou está incorreto")
+        return []
+
+
+class ActionDeleteCaseById(Action):
+
+    def name(self) -> Text:
+        return "action_delete_case_by_id"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        
+
+        id = str(tracker.get_slot('case_id'))
+        ong_id = str(tracker.get_slot('ong_id'))
+        print("Deletando caso..." + id)
+        
+        if(id and id != "" and id != "None"):
+            print("chamando a api em: ",API_PATH+ '/incidents/'+str(id),{"authorization":str(ong_id)})
+            r = requests.delete(API_PATH+ '/incidents/'+str(id),headers= {"authorization":str(ong_id)}) 
+            if(int(r.status_code)==404):
+                dispatcher.utter_message(text ="Caso não encontrado")
+                return
+            if(int(r.status_code)==500 or int(r.status_code)==401 ):
+                dispatcher.utter_message(text ="Pera que eu fiquei confuso.... deu um errinho aqui kk vamos de novo!")
+                return
+            
+            
+            dispatcher.utter_message(text ="Deletado!")
+        else:
+            dispatcher.utter_message(text ="Id não informado, ou está incorreto")
+
+        return []
